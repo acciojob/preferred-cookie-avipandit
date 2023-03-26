6512ecf85c46bde798f6f386e3008248fc178c88
AVINASH KUMAR <65335071+avipandit@users.noreply.github.com>
2023-03-26 17:09:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+var fontsize=document.getElementById("fontsize")
+var fontsize=document.getElementById("fontcolor")
+
+var myCookies=document.cookie.split("; ")
+
+myCookies.map((item)=>{
+	var cookieslist=item.split("=")
+})
+
+function onSubmit(event){
+	document.cookie=`fontsize=${fontsize.value}`
+	document.cookie=`fontcolor=${fontcolor.value}`
+	event.preventDefault();
+}
